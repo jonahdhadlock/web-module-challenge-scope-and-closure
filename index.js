69e9432ad7081f345a3c0d231e82fabc87c0ea0d
Jonah D. Hadlock <jonah-hadlock@lambdastudents.com>
2020-09-02 20:10:49 -0600

    re-formatted some things; still working on task1; confused by it

--- index.js
+++ index.js
@@ -1,26 +1,3 @@
-// ⭐️ Example Challenge START ⭐️
-
-/**
- * ### Challenge `processFirstItem`
- * 
- * @instructions
- * Implement a higher-order function called `processFirstItem`.
- * It takes two arguments:
- * @param stringList an array of strings.
- * @param callback function that takes a string as its argument.
- * @returns the result of invoking `callback` with the FIRST element in `stringList`.
- * 
- * Example of usage of this higher-order function:
- * Invoking `processFirstItem` passing `['foo', 'bar']` and `(str) => str + str`,
- * should return 'foofoo'.
- */
-function processFirstItem(stringList, callback) {
-    return callback(stringList[0])
-}
-
-// ⭐️ Example Challenge END ⭐️
-
-
 ///// M V P ///////
 
 /* Task 1: `counterMaker`
@@ -31,6 +8,7 @@ function processFirstItem(stringList, callback) {
  * 2. Which of the two uses a closure? How can you tell?
  * 
  * 3. In what scenario would the counter1 code be preferable? In what scenario would counter2 be better? 
+ * 
  *
  */
 
@@ -59,6 +37,8 @@ Write a function called `inning` that returns a random number of points that a t
 function inning() {
     return Math.round(Math.random() * 2);
 }
+
+console.log(inning());
 /* Task 3: finalScore()
 
 Write a higher order function called `finalScore` that accepts the callback function `inning` (from above) and a number of innings and and returns the final score of the game in the form of an object.
